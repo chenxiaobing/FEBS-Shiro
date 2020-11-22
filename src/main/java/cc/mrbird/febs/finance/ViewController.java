5dfd3970ce4ab985a3be709d9b954684a9e31a7f
jxcxb321@163.com <jxcxb321@163.com>
2020-11-22 23:51:24 +0800

    资产修改

--- src/main/java/cc/mrbird/febs/finance/ViewController.java
+++ src/main/java/cc/mrbird/febs/finance/ViewController.java
@@ -10,13 +10,22 @@ package cc.mrbird.febs.finance;
 
 import cc.mrbird.febs.common.controller.BaseController;
 import cc.mrbird.febs.common.entity.FebsConstant;
+import cc.mrbird.febs.common.utils.DateUtil;
 import cc.mrbird.febs.common.utils.FebsUtil;
+import cc.mrbird.febs.finance.mapper.PersonalAssetsMapper;
+import cc.mrbird.febs.system.entity.User;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @File: ViewController
@@ -28,6 +37,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(FebsConstant.VIEW_PREFIX + "finance")
 @RequiredArgsConstructor
 public class ViewController extends BaseController {
+    @Autowired
+    private PersonalAssetsMapper personalAssetsMapper;
 
     @GetMapping("personalAssets")
     public String online() {
@@ -35,7 +46,20 @@ public class ViewController extends BaseController {
     }
 
     @GetMapping("assetsAdd/{id}")
-    public String assetsAdd(@PathVariable String id) {
+    public String assetsAdd(@PathVariable String id, Model model) {
+        resolveUserModel(id, model, true);
         return FebsUtil.view("finance/assetsAdd");
+    }
+
+    private void resolveUserModel(String id, Model model, Boolean transform) {
+        if("-1".equals(id)){
+            return;
+        }
+        QueryWrapper<PersonalAssets> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(id)) {
+            queryWrapper.lambda().eq(PersonalAssets::getId, id);
+        }
+        List<PersonalAssets> list=personalAssetsMapper.selectList(queryWrapper);
+        PersonalAssets assets=list.get(0);
     }
 }
